54e7f36255b7eaacd9bf3e33a6a765be6743d429
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2021-11-06 21:40:38 +0800

    server_ip is not hardcode in tcp_handler anymore

--- client/src/lib/tcp_handler.c
+++ client/src/lib/tcp_handler.c
@@ -13,11 +13,16 @@
 #include "tcp_handler.h"
 
 #define COMMAND_SIZE sizeof(u_int16_t)
+#define SERVER_IP "./config/server_ip.txt"
 
 int sockfd = 0;
 uint8_t agv_id = 255;//default, should be renew after the first contact with server
 
 void tcp_init(){
+    
+    FILE *fp = open(SERVER_IP, "r");
+    char server_ip[32];
+    fgets(server_ip, 32, fp); 
     
     //socket setup
     sockfd = socket(AF_INET , SOCK_STREAM , 0);
@@ -28,7 +33,7 @@ void tcp_init(){
     struct sockaddr_in info;
     bzero(&info,sizeof(info));
     info.sin_family = PF_INET;
-    info.sin_addr.s_addr = inet_addr("192.168.0.123");
+    info.sin_addr.s_addr = inet_addr(server_ip);
     info.sin_port = htons(8700);
     
     if(connect(sockfd,(struct sockaddr *)&info,sizeof(info))==-1){
